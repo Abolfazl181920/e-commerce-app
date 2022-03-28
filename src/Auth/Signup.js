8066812fb9658041f5f783367bfcf19ddf47a2aa
Abolfazl <abolfazlm181920@gmail.com>
2022-03-28 21:55:40 +0430

    Add regestrations logics and using ReactToastify and Throw error with it and delete hooks folder and start to writting test

--- src/Auth/Signup.js
+++ src/Auth/Signup.js
@@ -1,23 +1,56 @@
+import { useState } from 'react'
 import { FormBodyFlex, FormBody, Inputs, InputsParent, FormTitle }  from './styles'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Button from './Button'
 
 const Signup = () => {
+
+    const [ name, setName ] = useState("")
+    const [ family, setFamily ] = useState("")
+    const [ email, setEmail ] = useState("")
+    const [ isValid, setIsValid ] = useState(false)
+
+    const submitForm = (e) => {
+        e.preventDefault()
+        if (!name, !family, !email) {
+           toast("Please Fill Out All Inputs!", {
+               theme:"dark"
+           })
+        } else {
+            setIsValid(true)
+            toast("Your regestration is completed!", {
+                theme:"dark"
+            })
+        }
+    }
+
     return (
         <FormBodyFlex>
-            <FormBody>
+            <ToastContainer/>
+            <FormBody action="#" onSubmit={submitForm}  data-testid="signup">
                 <FormTitle>Sign up</FormTitle>
                 <InputsParent>
                     <Inputs
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                         placeholder="&nbsp;&nbsp;&nbsp;Name"
                         margintop="9rem"
+                        data-testid="NameInput"
                     />
                     <Inputs
+                        value={family}
+                        onChange={(e) => setFamily(e.target.value)}
                         placeholder="&nbsp;&nbsp;&nbsp;Family"
                         margintop="1rem"
+                        data-testid="FamilyInput"
                     />
                     <Inputs
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         placeholder="&nbsp;&nbsp;&nbsp;Email"
                         margintop="1rem"
+                        data-testid="EmailInput"
                     />
                 </InputsParent>
                 <Button />
---
+++ src/Auth/Signup.test.js
@@ -0,0 +1,10 @@
+// import { render } from '@testing-library/react'
+// import Signup from './Signup'
+
+// describe("Signup component", () => {
+//     it("check to rendered Signup", () => {
+//         const { getByTestId } = render(<Signup />)
+//         const signup = getByTestId("signup")
+//         expect(signup).toBeTruthy()
+//     })
+// })
