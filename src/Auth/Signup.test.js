180e86a75539b3c68db7eeaa5de1dc5b0b8f8fd4
Abolfazl <abolfazlm181920@gmail.com>
2022-03-28 23:28:56 +0430

    do some test but it is not enought and I will complete it at (Mon, 25March)

--- src/Auth/Signup.test.js
+++ src/Auth/Signup.test.js
@@ -1,10 +1,16 @@
-// import { render } from '@testing-library/react'
-// import Signup from './Signup'
+import { render } from '@testing-library/react'
+import Signup from './Signup'
 
-// describe("Signup component", () => {
-//     it("check to rendered Signup", () => {
-//         const { getByTestId } = render(<Signup />)
-//         const signup = getByTestId("signup")
-//         expect(signup).toBeTruthy()
-//     })
-// })
+describe("Signup component", () => {
+    it("check to rendered Signup", () => {
+        const { getByTestId } = render(<Signup />)
+        const signup = getByTestId("signup")
+        expect(signup).toBeTruthy()
+    })
+
+    // it("Checking to be honest that name input don't be empty", () => {
+    //     const { getByTestId } = render(<Signup />)
+    //     const nameInput = getByTestId("NameInput")
+    //     expect(nameInput).toBeNull()
+    // })
+})
